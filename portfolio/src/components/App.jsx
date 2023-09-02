9871377be683b495a22e895073f420b7de1a957c
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-01 20:54:46 -0400

    Make App component detect window resizing

--- portfolio/src/components/App.jsx
+++ portfolio/src/components/App.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from 'react';
+import Header from './header/Header.jsx';
 
 function App() {
+  const [windowDimension, setWindowDimension] = useState(null);
+
+  useEffect(() => {
+    setWindowDimension(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimension(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const isMobile = windowDimension <= 640;
+
   return (
     <>
-      <h1>Hello World</h1>
+      <Header isMobile={isMobile} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
