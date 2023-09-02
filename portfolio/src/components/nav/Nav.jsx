ad42ffd957deea9bcffe02b11ddb5d6bb9aeaaf6
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-02 17:38:30 -0400

    Add click handler to close menu in mobile

--- portfolio/src/components/nav/Nav.jsx
+++ portfolio/src/components/nav/Nav.jsx
@@ -1,24 +1,28 @@
-const Nav = () => {
-  //const mobileStyles
+import PropTypes from 'prop-types';
 
+const Nav = ({ clickLinkHandler }) => {
   return (
     <nav>
       <ul className="nav__links">
-        <li>
+        <li onClick={clickLinkHandler}>
           <a href="#home">Home</a>
         </li>
-        <li>
+        <li onClick={clickLinkHandler}>
           <a href="#about">About</a>
         </li>
-        <li>
+        <li onClick={clickLinkHandler}>
           <a href="#projects">Projects</a>
         </li>
-        <li>
+        <li onClick={clickLinkHandler}>
           <a href="#contact">Contact</a>
         </li>
       </ul>
     </nav>
   );
+};
+
+Nav.PropTypes = {
+  clickLinkHandler: PropTypes.func,
 };
 
 export default Nav;
