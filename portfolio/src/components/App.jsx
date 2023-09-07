06dc28a67a8fe58469075670de9993a6f91f8b48
Dan McGrath <mcgrathd3254@yahoo.com>
2023-09-06 22:05:24 -0400

    Add projects to App

--- portfolio/src/components/App.jsx
+++ portfolio/src/components/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Header from './header/Header.jsx';
 import Hero from './hero/Hero.jsx';
 import About from './about/About.jsx';
+import Projects from './projects/Projects.jsx';
 import ContactForm from './contact/Contact.jsx';
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
       <main>
         <Hero />
         <About />
+        <Projects />
         <ContactForm />
       </main>
     </>
