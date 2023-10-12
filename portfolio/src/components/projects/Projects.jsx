9c6b87bda284b1120a76b026e5ff61f8623fb4b5
Dan McGrath <mcgrathd3254@yahoo.com>
2023-10-12 18:57:28 -0400

    update carousel styles

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -5,6 +5,29 @@ import imgURL from '../../assets/images/projectTemplate.jpg';
 import timeKiller from '../../assets/images/time-killer.png';
 
 const Projects = () => {
+  const arrowStyles = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+    borderRadius: '20px',
+    border: '1px solid black',
+    background: '#4b536c',
+  };
+
+  const indicatorStyles = {
+    background: 'hsl(120, 90%, 80%)',
+    opacity: '50%',
+    width: 8,
+    height: 8,
+    display: 'inline-block',
+    margin: '0 8px',
+    borderRadius: '10px',
+    cursor: 'pointer',
+  };
+
   const settings = {
     infiniteLoop: true,
     autoPlay: true,
@@ -60,7 +83,63 @@ const Projects = () => {
     <section className="section projects" id="projects">
       <h2 className="title">Projects</h2>
       <div className="wrapper">
-        <Carousel {...settings}>{arrayItems}</Carousel>
+        <Carousel
+          renderArrowPrev={(onClickHandler, hasPrev, label) =>
+            hasPrev && (
+              <button
+                type="button"
+                onClick={onClickHandler}
+                title={label}
+                style={{ ...arrowStyles, left: 15 }}
+              >
+                <i className="fa-solid fa-arrow-left"></i>
+              </button>
+            )
+          }
+          renderArrowNext={(onClickHandler, hasNext, label) =>
+            hasNext && (
+              <button
+                type="button"
+                onClick={onClickHandler}
+                title={label}
+                style={{ ...arrowStyles, right: 15 }}
+              >
+                <i className="fa-solid fa-arrow-right"></i>
+              </button>
+            )
+          }
+          renderIndicator={(onClickHandler, isSelected, index, label) => {
+            if (isSelected) {
+              return (
+                <li
+                  style={{
+                    ...indicatorStyles,
+                    background: '#000',
+                    opacity: '100%',
+                  }}
+                  aria-label={`Selected: ${label} ${index + 1}`}
+                  title={`Selected: ${label} ${index + 1}`}
+                />
+              );
+            }
+            return (
+              <li
+                style={indicatorStyles}
+                onClick={onClickHandler}
+                onKeyDown={onClickHandler}
+                value={index}
+                key={index}
+                role="button"
+                tabIndex={0}
+                title={`${label} ${index + 1}`}
+                aria-label={`${label} ${index + 1}`}
+              />
+            );
+          }}
+          {...settings}
+        >
+          {arrayItems}
+        </Carousel>
       </div>
     </section>
   );
