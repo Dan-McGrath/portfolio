01dcaed81977619e9d43e00c34e50aaf59189d6a
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-29 19:01:36 -0500

    remove slider

--- portfolio/src/components/projects/Projects.jsx
+++ portfolio/src/components/projects/Projects.jsx
@@ -1,41 +1,9 @@
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
 import Project from '../project/Project';
 import imgURL from '../../assets/images/projectTemplate.jpg';
 import timeKiller from '../../assets/images/time-killer.png';
 import arcanGaming from '../../assets/images/arcan-gaming.png';
 
 const Projects = () => {
-  const arrowStyles = {
-    position: 'absolute',
-    zIndex: 2,
-    top: 'calc(50% - 15px)',
-    width: 30,
-    height: 30,
-    cursor: 'pointer',
-    borderRadius: '20px',
-    border: '1px solid black',
-    background: '#4b536c',
-  };
-
-  const indicatorStyles = {
-    background: 'hsl(120, 90%, 80%)',
-    opacity: '50%',
-    width: 8,
-    height: 8,
-    display: 'inline-block',
-    margin: '0 8px',
-    borderRadius: '10px',
-    cursor: 'pointer',
-  };
-
-  const settings = {
-    infiniteLoop: true,
-    autoPlay: true,
-    stopOnHover: true,
-    swipeable: true,
-    showThumbs: false,
-  };
   const repoArray = [
     {
       name: 'Time Killer',
@@ -61,12 +29,6 @@ const Projects = () => {
       demo: 'https://dan-mcgrath.github.io/To-do/',
       img: imgURL,
     },
-    {
-      name: 'Weather App',
-      source: 'https://github.com/Dan-McGrath/Weather-App',
-      demo: 'https://dan-mcgrath.github.io/Weather-App/',
-      img: imgURL,
-    },
   ];
 
   const arrayItems = repoArray.map((ele) => (
@@ -83,65 +45,7 @@ const Projects = () => {
   return (
     <section className="section projects" id="projects">
       <h2 className="title">Projects</h2>
-      <div className="wrapper">
-        <Carousel
-          renderArrowPrev={(onClickHandler, hasPrev, label) =>
-            hasPrev && (
-              <button
-                type="button"
-                onClick={onClickHandler}
-                title={label}
-                style={{ ...arrowStyles, left: 15 }}
-              >
-                <i className="fa-solid fa-arrow-left"></i>
-              </button>
-            )
-          }
-          renderArrowNext={(onClickHandler, hasNext, label) =>
-            hasNext && (
-              <button
-                type="button"
-                onClick={onClickHandler}
-                title={label}
-                style={{ ...arrowStyles, right: 15 }}
-              >
-                <i className="fa-solid fa-arrow-right"></i>
-              </button>
-            )
-          }
-          renderIndicator={(onClickHandler, isSelected, index, label) => {
-            if (isSelected) {
-              return (
-                <li
-                  style={{
-                    ...indicatorStyles,
-                    background: '#000',
-                    opacity: '100%',
-                  }}
-                  aria-label={`Selected: ${label} ${index + 1}`}
-                  title={`Selected: ${label} ${index + 1}`}
-                />
-              );
-            }
-            return (
-              <li
-                style={indicatorStyles}
-                onClick={onClickHandler}
-                onKeyDown={onClickHandler}
-                value={index}
-                key={index}
-                role="button"
-                tabIndex={0}
-                title={`${label} ${index + 1}`}
-                aria-label={`${label} ${index + 1}`}
-              />
-            );
-          }}
-          {...settings}
-        >
-          {arrayItems}
-        </Carousel>
-      </div>
+      <div className="wrapper">{arrayItems}</div>
     </section>
   );
 };
